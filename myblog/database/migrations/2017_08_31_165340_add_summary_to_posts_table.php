89bf5a0159f7e122fa89b8ab2a91169fe5efa84c
riohik@gmail.com <riohik@gmail.com>
2017-09-13 13:24:41 +0000

    test

---
+++ myblog/database/migrations/2017_08_31_165340_add_summary_to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSummaryToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('summary');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('summary');
+            //
+        });
+    }
+}
